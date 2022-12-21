5253f6840b5c15ed47a85cc0213f1275ac369989
tom-urkin <tom.urkin@gmail.com>
2022-12-21 16:58:47 +0200

    Push Fibonacci_LFSR files

--- Fibonacci_LFSR.sv
+++ Fibonacci_LFSR.sv
@@ -1,18 +1,18 @@
 //Fibonacci Linear Feedback Shift Register (LFSR)
 
-module Fibinacci_LFSR(rst,clk,seed,taps);
+module Fibonacci_LFSR(rst,clk,seed,data);
 
 //Parameter declerations
-LENGTH=16; 
+parameter LENGTH=16; 								//LFSR length
+parameter TAPS=53256;								//TAP locations not including the XOR operation with the right-most bit. Deafutls is the polynomial for maximal 16-bit LFSR 
 
 //Inputs
 input logic rst;                            //Active high logic
 input logic clk;                            //Input clock
 input logic [LENGTH-1:0] seed;              //Input seed - the initial state of the LFSR
-input logic [LENGTH-1:0] taps;              //Taps locations represented by a binary word of width LENGTH
-
+integer k;
 //Outputs
-output logic [LENGTH-1:0] data;            //This is the output word
+output logic [LENGTH-1:0] data;             //This is the output word
 
 //Internal signals
 logic feedback_bit;                         //Input to the leftmost flip-flop
@@ -23,9 +23,9 @@ always @(posedge clk or negedge rst)
     if (!rst)
         data<=seed;
     else
-        data<={feedback_bit,data[LENGTH-1:1]}
+        data<={feedback_bit,data[LENGTH-1:1]};
 
 //Calculating the feedback bit
-assign feedback_bit = ^(taps&data);
+assign feedback_bit = ^{(TAPS[LENGTH-1:0]&data),data[0]};
 
 endmodule 
---
+++ Fibonacci_LFSR_TB.sv
@@ -0,0 +1,44 @@
+`timescale 1ns/100ps
+//
+module Fibonacci_LFSR_TB();
+
+//Parameter declarations
+parameter CLK_PERIOD = 20;
+parameter LENGTH = 16;                        //Word width
+parameter TAPS = 53256;
+//Internal signals declarations
+logic clk;
+logic rst;
+logic [LENGTH-1:0] data;
+//integer k;
+
+//Fibonacci LFSR instantiation
+Fibonacci_LFSR #(.LENGTH(LENGTH), .TAPS(TAPS)) U1(
+                .clk(clk),
+                .rst(rst),
+                .seed(16'd1),
+				.data(data)
+                );
+
+//Initial blocks
+initial
+begin
+    rst=1'b0;
+    clk=1'b0;
+    @(posedge clk);
+    rst=1'b1;
+
+	repeat (100000)
+    @(posedge clk);
+	$finish;
+end
+
+//Clock generation (50MHz)
+always
+begin
+#(CLK_PERIOD/2);
+clk=~clk;
+end
+
+
+endmodule
