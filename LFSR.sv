f3b09def03ae40c39792602e766e8a0d5d13145a
tom-urkin <tom.urkin@gmail.com>
2022-12-25 12:11:54 +0200

    Edited source files

--- LFSR.sv
+++ LFSR.sv
@@ -1,27 +1,26 @@
-//Fibonacci Linear Feedback Shift Register (LFSR)
+//Fibonacci ('many-to-one')/Galois ('one-to-many') Linear Feedback Shift Registers (LFSR)
 
 module LFSR(rst,clk,seed,data);
 
 //Parameter declerations
-parameter TYPE = 1;                                     //LFSR type: 0 for Fibonacci and 1 for Galois
+parameter TYPE = 0;                                     //LFSR type: 0 for Fibonacci and 1 for Galois. Deafault is Fibonacci LFSR.
 parameter LENGTH=16;                                    //LFSR structure: [0][1][2]--->[LENGTH]
-//parameter TAPS=16'b0110100000000001;                  //TAP locations. Deafutls is the polynomial for maximal 16-bit LFSR 
-parameter TAPS=16'b0110100000000000; 						  //TAP locations for Galios
+parameter TAPS=16'b0110100000000001;                    //TAP locations. Deafutls is the polynomial for maximal 16-bit Fibonacci LFSR 
 //Inputs
 input logic rst;                                        //Active high logic
 input logic clk;                                        //Input clock
 input logic [0:LENGTH-1] seed;                          //Input seed - the initial state of the LFSR
 integer k;
 //Outputs
-output logic [0:LENGTH-1] data;                         //This is the output word
+output logic [0:LENGTH-1] data;                         //Output word
 
 //Internal signals
-logic feedback_bit;                                     //Input to the leftmost flip-flop
-logic [0:LENGTH-1] data_tmp;
+logic feedback_bit;                                     //Input to the leftmost flip-flop (Fibonacci LFSR)
+logic [0:LENGTH-1] data_tmp;									  //Right-most bit XORed with the data vector as a function of tap locations (Galois LFSR)
 
 //HDL code
 generate
-	if (TYPE==0)        			                            //Fibonacci LFSR
+	if (TYPE==0)        			                           //Instantiate a Fibonacci LFSR
 	begin
 		//Shift register
 		always @(posedge clk or negedge rst)
@@ -33,16 +32,17 @@ generate
 		//Calculating the feedback bit
 		assign feedback_bit = ^(TAPS&data);
 	end
-else                                                   //Galois LFSR
+else                                                	   //Instantiate a Galois LFSR
 	begin
 	
-	assign data_tmp=(TAPS&{LENGTH{data[LENGTH-1]}})^(data);
+		assign data_tmp=(TAPS&{LENGTH{data[LENGTH-1]}})^(data);
 	
-	always @(posedge clk or negedge rst)
-		if (!rst)
-			data<=seed;
-		else
-			data<={data_tmp[15],data_tmp[0:14]};
+		//Shift register
+		always @(posedge clk or negedge rst)
+			if (!rst)
+				data<=seed;
+			else
+				data<={data_tmp[LENGTH-1],data_tmp[0:LENGTH-2]};
 	end
 endgenerate
 
--- LFSR_TB.sv
+++ LFSR_TB.sv
@@ -2,23 +2,29 @@
 //
 module LFSR_TB();
 //Parameter declarations
-parameter CLK_PERIOD = 20;
-parameter LENGTH = 16;                        //Word width
-parameter TAPS_FIBONACCI = 16'b0110100000000001;		  //[0][1][2]----[15]
-parameter TAPS_GALOIS = 16'b0110100000000000;		  //[0][1][2]----[15]
 parameter FIBONACCI = 0;
 parameter GALOIS = 1;
-//Internal signals declarations
-logic clk;
-logic rst;
-logic [0:LENGTH-1] data;
-//integer k;
+parameter LENGTH = 8;                                 //Word width
 
-//Fibonacci LFSR instantiation
-LFSR #(.LENGTH(LENGTH), .TAPS(TAPS_GALOIS), .TYPE(GALOIS)) U1(
+//parameter TAPS_FIBONACCI = 16'b0110100000000001;    //[0][1][2]----[15]. Use for 16-bit LFSR
+parameter TAPS_FIBONACCI = 8'b01110001;               //[0][1][2]----[7]. Use fir 8-bit LFSR
+
+//parameter TAPS_GALOIS =    16'b0110100000000000;      //[0][1][2]----[15]. Use for 16-bit LFSR
+parameter TAPS_GALOIS =    8'b01110000;       		  //[0][1][2]----[7]. Use for 8-bit LFSR
+
+parameter CLK_PERIOD = 20;
+
+
+//Internal signals declarations
+logic clk;                                             //LFSR clock
+logic rst;                                             //Active high logic
+logic [0:LENGTH-1] data;                               //Output of the LFSR module
+
+//LFSR instantiation
+LFSR #(.LENGTH(LENGTH), .TAPS(TAPS_FIBONACCI), .TYPE(FIBONACCI)) U1(
                 .clk(clk),
                 .rst(rst),
-                .seed(16'd1),
+                .seed(8'd1),
 				.data(data)
                 );
 
